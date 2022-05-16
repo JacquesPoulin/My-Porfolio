67c908ac44a16422bd53b7b1fc9a8c8484069ccf
JacquesPoulin <jacques.poulin64@gmail.com>
2022-05-16 11:02:26 +0200

    [ROUTE] : Vercel 1st deployment test

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="flex items-center justify-center w-screen h-screen text-2xl text-white bg-slate-500 text-shadow-md">
       <h1>LOADING ...</h1>
+      <img
+        src="../public/assets/images/svg/react-svgrepo-com.svg"
+        alt="Logo React"
+      />
     </div>
   );
 }
