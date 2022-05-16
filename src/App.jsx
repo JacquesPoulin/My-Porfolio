692bef49d0b7ad9a6b554c4e4ede9e4f1ac123d6
JacquesPoulin <jacques.poulin64@gmail.com>
2022-05-16 11:24:50 +0200

    [ROUTE] : Vercel 2nd deployment test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import React from "react";
-// import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Landing from "./pages/Landing";
+import About from "./pages/About";
+import Skill from "./pages/Skill";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen text-2xl text-white bg-slate-500 text-shadow-md">
-      <h1>LOADING ...</h1>
-      <img
-        src="../public/assets/images/svg/react-svgrepo-com.svg"
-        alt="Logo React"
-      />
+    <div className="App">
+      <HashRouter>
+        <NavBar />
+        <Routes>
+          <Route path="*" element={<Landing />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/aboutme" element={<About />} />
+          <Route path="/myskills" element={<Skill />} />
+          <Route path="/myprojects" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <img
+          src="../public/assets/images/svg/node-svgrepo-com.svg"
+          alt="Logo NodeJS"
+        />
+        <Footer />
+      </HashRouter>
     </div>
   );
 }
