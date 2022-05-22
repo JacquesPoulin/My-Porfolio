1c3a35fd5814d78e6cb878e9e87b5f427c7ea44c
JacquesPoulin <jacques.poulin64@gmail.com>
2022-05-22 21:53:01 +0200

    [STYLE] : styling and animations added to multiple component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,8 +7,21 @@ const Contact = () => {
       name="Contact"
       className="w-full h-full flex text-slate-50 bg-slate-900"
     >
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col md:flex w-full h-full bg-slate-900 bp1:mt-28 md:mt-28">
+      <div className="max-w-[700px] p-4 mx-auto flex flex-col md:flex w-full h-full bg-slate-900 bp1:mt-28 md:mt-28">
         <ContactForm />
+        <div className="w-full flex justify-center">
+          <p className="text-xs"> made with</p>
+          <img
+            src="/assets/images/svg/react-svgrepo-com.svg"
+            alt="logo REACT"
+            className="w-4 h-4 ml-2"
+          />
+          <img
+            src="../../public/assets/images/svg/tailwindcss-icon-svgrepo-com.svg"
+            alt="logo REACT"
+            className="w-4 h-4 ml-2"
+          />
+        </div>
       </div>
     </div>
   );
