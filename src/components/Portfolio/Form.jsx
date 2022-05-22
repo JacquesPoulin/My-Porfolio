5fab6281ed2269a4ede60b09a2be1c759bf88c11
JacquesPoulin <jacques.poulin64@gmail.com>
2022-05-22 11:16:23 +0200

    [FEATURES] : Form Component logic done

--- src/components/Portfolio/Form.jsx
+++ src/components/Portfolio/Form.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React from "react";
 import PropTypes from "prop-types";
 
@@ -5,20 +6,24 @@ import PropTypes from "prop-types";
 import Select from "react-select";
 
 // ----- Datas -----
-import technos from "../../../data/technos";
+import technos from "../../../data/stackList";
 
 const Form = ({ selected, setSelected }) => {
   return (
-    <form className="w-full flex justify-around mb-4">
+    <form className="w-full flex justify-between mb-4">
       <Select
-        placeholder="Technos & langages"
-        options={technos}
+        placeholder="Stack ..."
+        options={technos.sort((a, b) =>
+          a.label > b.label ? 1 : b.label > a.label ? -1 : 0
+        )}
         value={selected}
         onChange={(e) => setSelected(e.value)}
         className="w-60 text-slate-900 rounded-lg box-shadow-1 bg-slate-50 dark:bg-slate-900 dark:bg-opacity-[10%] bg-opacity-[10%] font-exo tracking-wide outline-none bp2:w-[45%] bp3:w-[60%] bp4:w-[90%]"
       />
-      <button type="button">TEST</button>
-      <button type="button">TEST</button>
+
+      <button type="button" onClick={() => setSelected("Toutes")}>
+        Voir tout
+      </button>
     </form>
   );
 };
