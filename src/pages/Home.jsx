41dda4d0f89b761648949eb9e0a7ddf5d7863f62
JacquesPoulin <jacques.poulin64@gmail.com>
2022-06-12 21:43:03 +0200

    [UPDATES] : new favicon, deleted unused svg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Presentation from "../components/Home/Presentation";
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-slate-900">
+    <div className="w-full h-screen text-slate-50 bg-slate-900">
       <Presentation />
     </div>
   );
